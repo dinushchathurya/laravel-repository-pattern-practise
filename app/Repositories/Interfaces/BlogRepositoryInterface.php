a2361f1993db218fc68b5777a86de5059d0be019
dinushchathurya <dinushchathurya21@gmail.com>
2021-04-03 13:42:29 +0530

    feat: create BlogRepositoryInterface.php file

---
+++ app/Repositories/Interfaces/BlogRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\User;
+
+interface BlogRepositoryInterface
+{
+    public function all();
+
+    public function getByUser(User $user);
+    
+}
